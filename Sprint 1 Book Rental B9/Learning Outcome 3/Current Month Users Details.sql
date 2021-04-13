473cbb75f1ed24f05227155575dcb3ff45938463
alanjj22 <alanjomyjosephstm@gmail.com>
2021-04-13 11:57:51 +0530

    outcome3 2 chage

--- Sprint 1 Book Rental B9/Learning Outcome 3/Current Month Users Details.sql
+++ Sprint 1 Book Rental B9/Learning Outcome 3/Current Month Users Details.sql
@@ -4,4 +4,4 @@
  select * from user_details 
  where user_id in (
     select user_id from book_rental
-    where rental_start_date between '01/Dec/2020' and '31/Dec/2020');
+    where to_char(rental_start_date, 'Mon') = 'Dec');
